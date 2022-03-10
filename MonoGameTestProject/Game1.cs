395c88f477ce6cb2c1bac4ebce3550e0e6cf9b23
Justin Payne <justinthecore@gmail.com>
2022-03-10 13:52:33 -0800

    Add project files.

---
+++ MonoGameTestProject/Game1.cs
@@ -0,0 +1,136 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+/// <summary>
+/// Project prototype to test game creation and player creation using MonoGame Framework
+/// Authors: Team 4 - Jason Cheung, Gabriel Baluyut, Justin Payne
+/// Date: Mar 3, 2022
+/// Source: https://docs.monogame.net/articles/getting_started/0_getting_started.html
+/// </summary>
+namespace MonoGameTestProject
+{
+    /// <summary>
+    /// Game Class definition - main class program of the MonoGame project.
+    /// This class needs to include these methods necessary for the game to run:
+    ///  - Constructor & key variables
+    ///  - Initialize method
+    ///  - LoadContent & UnloadContent method
+    ///  - Update method
+    ///  - Draw method
+    /// </summary>
+    public class Game1 : Game
+    {
+        // Simple ball assset to use as a player object
+        Texture2D ballTexture;
+        // Adding position and speed for ball (player) asset
+        Vector2 ballPosition;
+        float ballSpeed;
+
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+
+        /// <summary>
+        /// Game Constructor.
+        /// includes bits to tell the project how to start.
+        /// </summary>
+        public Game1()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+        }
+
+        /// <summary>
+        /// Initialize method - Initialize the game upon its startup.
+        /// </summary>
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+            ballPosition = new Vector2(_graphics.PreferredBackBufferWidth / 2,
+                _graphics.PreferredBackBufferHeight / 2);
+            ballSpeed = 400f;
+
+            base.Initialize();
+        }
+
+        /// <summary>
+        /// LoadContent Method - Add assets from the running game from the Content project.
+        /// note: there is also an UnloadContent() method which is currently unused.
+        /// </summary>
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            // TODO: use this.Content to load your game content here
+            ballTexture = Content.Load<Texture2D>("ball");
+        }
+
+        /// <summary>
+        /// Update Method - Called on a regular interval to update the game state.
+        /// Updating game state includes taking player input, checking for collision, playing audio, animating entities, etc...
+        /// This method is called multiple times per second.
+        /// </summary>
+        /// <param name="gameTime"></param>
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            // TODO: Add your update logic here
+            var kstate = Keyboard.GetState();
+
+            if (kstate.IsKeyDown(Keys.Up))
+                ballPosition.Y -= ballSpeed * (float)gameTime.ElapsedGameTime.TotalSeconds;
+
+            if (kstate.IsKeyDown(Keys.Down))
+                ballPosition.Y += ballSpeed * (float)gameTime.ElapsedGameTime.TotalSeconds;
+
+            if (kstate.IsKeyDown(Keys.Left))
+                ballPosition.X -= ballSpeed * (float)gameTime.ElapsedGameTime.TotalSeconds;
+
+            if (kstate.IsKeyDown(Keys.Right))
+                ballPosition.X += ballSpeed * (float)gameTime.ElapsedGameTime.TotalSeconds;
+
+            if (ballPosition.X > _graphics.PreferredBackBufferWidth - ballTexture.Width / 2)
+                ballPosition.X = _graphics.PreferredBackBufferWidth - ballTexture.Width / 2;
+            else if (ballPosition.X < ballTexture.Width / 2)
+                ballPosition.X = ballTexture.Width / 2;
+
+            if (ballPosition.Y > _graphics.PreferredBackBufferHeight - ballTexture.Height / 2)
+                ballPosition.Y = _graphics.PreferredBackBufferHeight - ballTexture.Height / 2;
+            else if (ballPosition.Y < ballTexture.Height / 2)
+                ballPosition.Y = ballTexture.Height / 2;
+
+            base.Update(gameTime);
+        }
+
+        /// <summary>
+        /// Draw Method - Called on a regular interval to take the current 
+        /// game state and draw the game entities to the screen.
+        /// This method is called multiple times per second.
+        /// </summary>
+        /// <param name="gameTime"></param>
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            // TODO: Add your drawing code here
+            _spriteBatch.Begin();
+            _spriteBatch.Draw(
+                ballTexture,
+                ballPosition,
+                null,
+                Color.White,
+                0f,
+                new Vector2(ballTexture.Width / 2, ballTexture.Height / 2),
+                Vector2.One,
+                SpriteEffects.None,
+                0f
+            );
+            _spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+    }
+}
