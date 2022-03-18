1dbdc260462563dfe36f073bb3a2c5fe684dbafd
JasonCheung <jason.cheung@live.ca>
2022-03-17 17:55:10 -0700

    Created a Camera class to follow the player using a TransformMatrix.

---
+++ MonoGameTestProject/Source/Engine/Camera.cs
@@ -0,0 +1,41 @@
+﻿#region Includes
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using System.Xml.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+#endregion
+
+namespace Jusgabon
+{
+    public class Camera
+    {
+        public Matrix Transform { get; private set; }
+
+        public void Follow(Sprite target)
+        {
+            var position = Matrix.CreateTranslation(
+                -target.Position.X - (target.Rectangle.Width / 2),
+                -target.Position.Y - (target.Rectangle.Height / 2),
+                0);
+
+            var offset = Matrix.CreateTranslation(
+                    Game1.screenWidth / 2,
+                    Game1.screenHeight / 2,
+                    0);
+
+            Transform = position * offset;
+        }
+
+
+    }
+}
