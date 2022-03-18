559510bf52118a7e9c15966a44e983a13d63d214
JasonCheung <jason.cheung@live.ca>
2022-03-17 17:53:58 -0700

    Created parent Sprite/Entity class.

---
+++ MonoGameTestProject/Source/Sprites/Sprite.cs
@@ -0,0 +1,48 @@
+﻿#region Includes
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using System.Xml.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+#endregion
+
+namespace Jusgabon
+{
+    public class Sprite : Component
+    {
+        protected Texture2D _texture;
+
+        public Vector2 Position { get; set; }
+
+
+
+        public Rectangle Rectangle 
+        { 
+            get { return new Rectangle((int)Position.X, (int)Position.Y, _texture.Width, _texture.Height); } 
+        }
+
+        public Sprite(Texture2D texture)
+        {
+            _texture = texture;
+        }
+        
+        public override void Update(GameTime gameTime)
+        {
+            
+        }
+
+        public override void Draw(GameTime gameTime, SpriteBatch spriteBatch)
+        {
+            Globals.spriteBatch.Draw(_texture, Position, Color.White);
+        }
+    }
+}
