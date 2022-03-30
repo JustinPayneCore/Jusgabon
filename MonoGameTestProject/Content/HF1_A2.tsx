230930ba4680f8a805a6ade30cafdb33d122dc1c
Gabriel Baluyut <60728482+gabeBaluyut@users.noreply.github.com>
2022-03-30 00:10:35 -0700

    Added missing files for tilemap

---
+++ MonoGameTestProject/Content/HF1_A2.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.2" name="HF1_A2" tilewidth="32" tileheight="32" tilecount="143" columns="11">
+ <image source="../NinjaAdventure/Backgrounds/Tilesets/TilesetFloor.png" width="352" height="417"/>
+</tileset>
