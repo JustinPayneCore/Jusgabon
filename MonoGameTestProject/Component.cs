3880b6ca3a75965253f2e3c38e2ec16e6053b627
Gabriel Baluyut <60728482+gabeBaluyut@users.noreply.github.com>
2022-04-05 23:57:50 -0700

    Added button, associated classes, and Component class

---
+++ MonoGameTestProject/Component.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Jusgabon
+{
+    public abstract class ComponentMonogame
+    {
+
+        public abstract void Draw(GameTime gameTime, SpriteBatch spriteBatch);
+
+        public abstract void Update(GameTime gameTime);
+    }
+}
